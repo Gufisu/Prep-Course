1b2eefdb2c3062aec10fa4e4c1b42d4268f1e87a
Gustavo Figueroa Suárez <gufisu@homail.com>
2022-02-09 22:15:08 -0500

    second commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = null;
 const nuevoNum = null;
 
 // Crea una variable booleana:
-const nuevoBool = "ls rm";
+const nuevoBool = "ls rdfsdfsdfsdfsdffdm";
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
